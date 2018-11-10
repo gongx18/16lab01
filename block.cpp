b4eeaa73724167a1d551851eaa23d64a177f4e93
Xi Gong <xi@ucsb.edu>
2018-11-10 10:38:45 -0800

    version 1.0

---
+++ block.cpp
@@ -0,0 +1,26 @@
+//block.cpp
+#include <iostream>
+using namespace std; 
+int main(){
+	int r; 
+	int c; 
+	cout<<"Enter number of rows and columns:"<<endl; 
+	cin>> r; 
+	cin>> c; 
+
+
+	while(r != 0 && c != 0){
+		for(int i = 0; i< r; i ++){
+			int j = 0; 
+			while(j != c){
+				cout << "X";
+				j++;  
+			} 
+			cout << "\n";
+		} 
+		cout<<"Enter number of rows and columns:"<<endl; 
+		cin>> r; 
+		cin>> c; 
+	}
+	return 0; 	
+}
---
+++ pi.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std; 
+int main(){
+
+	int input = 0;  
+
+	cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl; 
+	cin >> input; 
+
+	while(input!=-1){
+		double s = 1; 
+		double sum = 0;
+		double denominator = 1;  
+		for (int i=0; i<input; i++){
+			sum += (4/denominator)*s;
+			denominator += 2; 
+			s = -s; 
+		}
+
+		cout.setf(ios::fixed); 	   // Display in fixed point notation. For example, display 1e-1 as 0.1
+		cout.setf(ios::showpoint); // Always display the decimal point.
+		cout.precision(3);         // Set the number of digits to display after the decimal point to 3
+
+		cout << "The approximate value of pi using "<< input <<" term is:" << sum << endl; 
+		cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl; 
+		cin >> input; 
+	}
+	return 0; 
+	
+}
+
