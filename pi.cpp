d0bb69ca0228ed308e9ec653ba1e8502b7213bfc
Xi Gong <xi@ucsb.edu>
2018-11-13 19:16:13 -0800

    Version 3.0

--- pi.cpp
+++ pi.cpp
@@ -3,9 +3,8 @@
 using namespace std; 
 int main(){
 
-	int input = 0;  
-
-	cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl; 
+	int input = 0; 
+	cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl;
 	cin >> input; 
 
 	while(input!=-1){
@@ -21,8 +20,12 @@ int main(){
 		cout.setf(ios::fixed); 	   // Display in fixed point notation. For example, display 1e-1 as 0.1
 		cout.setf(ios::showpoint); // Always display the decimal point.
 		cout.precision(3);         // Set the number of digits to display after the decimal point to 3
-
-		cout << "The approximate value of pi using "<< input + 1 <<" terms is: " << sum << endl; 
+		if(input == 0){
+		cout << "The approximate value of pi using "<<input + 1 <<" term is: "<< sum << endl;
+		}
+		else{
+		cout <<"The approximate value of pi using "<<input + 1 <<" terms is: "<<sum<<endl; 
+		} 
 		cout << "Enter the value of the parameter 'n' in the Leibniz formula (or -1 to quit):" << endl; 
 		cin >> input; 
 	}
